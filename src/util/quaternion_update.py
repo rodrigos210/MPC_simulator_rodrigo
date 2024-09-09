6da94cf9f0fe7f9690ef5acf5514165e23c96629
rodrigos210 <147573833+rodrigos210@users.noreply.github.com>
2024-09-09 15:05:18 +0200

    added a quaternion update function for numpy

--- src/util/quaternion_update.py
+++ src/util/quaternion_update.py
@@ -1,22 +1,31 @@
 import casadi as ca
+import numpy as np
 
-def next_quaternion(omegas, dt, Omega, quat):
-    assert Omega.shape == (4, 4), "Omega must be a 4x4 CasADi symbolic matrix"
-    assert quat.shape == (4, 1), "Quaternion must be a 4x1 CasADi symbolic matrix"
+def quaternion_update_ca(quat, omegas, dt):
+        
+# Closed-Form quaternion update (https://ahrs.readthedocs.io/en/latest/filters/angular.html)
+# Expanded Skew-Symmetric Omega Matrix
+    Omega = ca.vertcat(
+        ca.horzcat(0, -omegas[0], -omegas[1], -omegas[2]),
+        ca.horzcat(omegas[0], 0, omegas[2], -omegas[1]),
+        ca.horzcat(omegas[1], -omegas[2], 0, omegas[0]),
+        ca.horzcat(omegas[2], omegas[1], -omegas[0], 0)
+    )
+
+    next_quat = ca.mtimes(ca.cos(0.5 * ca.norm_2(omegas) * dt) * ca.MX.eye(4) + (1/ca.norm_2(omegas)) * ca.sin(0.5 * ca.norm_2(omegas) * dt) * Omega, quat)
+    next_quat = next_quat/ca.norm_2(next_quat)
+    return next_quat
+
+def quaternion_update_np(quat, omegas, dt):
+    omega1, omega2, omega3 = omegas
+        
+# Closed-Form quaternion update (https://ahrs.readthedocs.io/en/latest/filters/angular.html)
+# Expanded Skew-Symmetric Omega Matrix
+    Omega = np.array([[0, -omega1, -omega2, -omega3],
+                      [omega1, 0, omega3, -omega2],
+                      [omega2, -omega3, 0, omega1],
+                      [omega3, omega2, -omega1, 0]])
     
-    omega_norm = ca.norm_2(omegas)
-    
-
-    cos_term = ca.cos(0.5 * omega_norm * dt)
-    sin_term = ca.sin(0.5 * omega_norm * dt)
-    
-    # Handle the division by zero case for omega_norm
-    # norm_factor = ca.if_else(omega_norm != 0, omega_norm / 10, 0)
-    #rotation_matrix = ca.if_else(omega_norm == 0,ca.MX.eye(4), cos_term * ca.MX.eye(4) + omega_norm * sin_term * Omega)
-    rotation_matrix = ca.if_else(omega_norm != 0, cos_term * ca.MX.eye(4) + omega_norm * sin_term * Omega, ca.MX.eye(4))
-
-    next_quat = ca.mtimes(rotation_matrix, quat)
-    # quat_update = ca.mtimes(rotation_matrix, quat)
-    # next_quat = ca.if_else(omega_norm == 0, quat, quat_update)
-
+    next_quat = (np.cos(0.5 * np.linalg.norm(omegas) * dt) * np.eye(4) + (1/np.linalg.norm(omegas)) * np.sin(0.5 * np.linalg.norm(omegas) * dt) * Omega) @ quat
+    next_quat = next_quat/np.linalg.norm(next_quat)
     return next_quat
